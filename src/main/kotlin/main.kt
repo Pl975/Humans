948e30f642f4baec75295688c6fc1722c844adb8
Pavel <pmshuv975@gmail.com>
2024-06-02 18:32:44 +0300

    Solution of Task 2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,2 +1,9 @@
 package ru.netology
 
+fun main() {
+    val likes = 1201
+    val StringEnd = if (likes % 10 == 1 && likes % 100 != 11) "человеку" else "людям"
+
+    println("Понравилось $likes $StringEnd")
+
+}
